cd5534fe54a193c08c05b3ac03219e1bec016f5b
Deepika-reddy88 <deepikareddyyedavally@gmail.com>
2026-05-04 19:08:19 +0530

    programs

---
+++ fact.js
@@ -0,0 +1,6 @@
+let a=6;
+let fac=1;
+for(let i=1;i<=a;i++){
+    fac=fac*i;
+}
+console.log(fac);
---
+++ stringrev.js
@@ -0,0 +1,7 @@
+let num=2678;
+let rev=0;
+for(;num>0;num=Math.floor(num/10)){
+    let rem=num%10;
+    rev=rev*10+rem;
+}
+console.log(rev);
